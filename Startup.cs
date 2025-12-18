33aad9ad580463bff5309f40a6d8d0b8c1cdbba0
Muhammad Hamad Mehdi <hamadmehdi1214@outlook.com>
2025-12-18 16:53:51 +0500

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PrayerTracker1.Startup))]
+namespace PrayerTracker1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
